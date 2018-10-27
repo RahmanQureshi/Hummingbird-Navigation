60f86830861b455d51458ac6605ffd6c6770cf97
John Doe <vuwij@me.com>
2018-10-28 04:33:04 +0900

    Fixed simulink files

--- hummingbird_msgs/matlab_gen/build/rosjava_build/mavros_msgs/build/generated-src/mavros_msgs/CommandLong$Service.java
+++
@@ -1,8 +0,0 @@
-package mavros_msgs;
-
-public interface CommandLong$Service extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "mavros_msgs/CommandLong$Service";
-  static final java.lang.String _DEFINITION = "# Generic COMMAND_LONG\n\nbool broadcast # send this command in broadcast mode\n\nuint16 command\nuint8 confirmation\nfloat32 param1\nfloat32 param2\nfloat32 param3\nfloat32 param4\nfloat32 param5\t# x_lat\nfloat32 param6\t# y_lon\nfloat32 param7\t# z_alt\n---\nbool success\n# raw result returned by COMMAND_ACK\nuint8 result\n";
-  static final boolean _IS_SERVICE = true;
-  static final boolean _IS_ACTION = false;
-}
--- hummingbird_msgs/matlab_gen/build/rosjava_build/mavros_msgs/build/generated-src/mavros_msgs/WaypointSetCurrent$Service.java
+++
@@ -1,8 +0,0 @@
-package mavros_msgs;
-
-public interface WaypointSetCurrent$Service extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "mavros_msgs/WaypointSetCurrent$Service";
-  static final java.lang.String _DEFINITION = "# Request set current waypoint\n#\n# wp_seq - index in waypoint array\n\nuint16 wp_seq\n---\nbool success\n";
-  static final boolean _IS_SERVICE = true;
-  static final boolean _IS_ACTION = false;
-}
